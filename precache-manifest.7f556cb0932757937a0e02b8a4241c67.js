81af8e50aca6bae06ae4bd83a8ee5a317b4346b6
justinf608 <justinf608@gmail.com>
2019-02-15 22:03:51 -0600

    Updates

---
+++ precache-manifest.7f556cb0932757937a0e02b8a4241c67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39d2babdc9fea98591c",
+    "url": "/justinf608/reactArrowverseApp/static/js/runtime~main.a39d2bab.js"
+  },
+  {
+    "revision": "4f4b986dfabf9d91cb4c",
+    "url": "/justinf608/reactArrowverseApp/static/js/main.4f4b986d.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/justinf608/reactArrowverseApp/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "4f4b986dfabf9d91cb4c",
+    "url": "/justinf608/reactArrowverseApp/static/css/main.4f7c6cae.chunk.css"
+  },
+  {
+    "revision": "1ba69a95c8d4a705c76bae0a7b52148b",
+    "url": "/justinf608/reactArrowverseApp/index.html"
+  }
+];
